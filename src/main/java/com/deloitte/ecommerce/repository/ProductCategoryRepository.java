43af5b70925ee0fa24768fb769ff97d634c04d7a
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-31 12:23:41 -0300

    ajuste cors

--- src/main/java/com/deloitte/ecommerce/repository/ProductCategoryRepository.java
+++ src/main/java/com/deloitte/ecommerce/repository/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.deloitte.ecommerce.repository;
 import com.deloitte.ecommerce.domain.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 //@ de customizacao: productCategory é o nome do JSON de entrada e o path /product-category
 @RepositoryRestResource(collectionResourceRel = "productCategory", path="product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
--- src/main/java/com/deloitte/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/deloitte/ecommerce/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.deloitte.ecommerce.repository;
 
 import com.deloitte.ecommerce.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
