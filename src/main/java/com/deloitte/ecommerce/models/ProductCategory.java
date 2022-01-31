aaaf6b0858a3a020fea25f2f4a0459f9193d7268
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-01-31 10:44:29 -0300

    entidade categoria e relacoes

---
+++ src/main/java/com/deloitte/ecommerce/models/ProductCategory.java
@@ -0,0 +1,28 @@
+package com.deloitte.ecommerce.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="product_category")
+
+//pelo jeito tem bug no @Data quando ha relacao de one to many
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy="category")
+    private Set<Product> products;
+
+}
