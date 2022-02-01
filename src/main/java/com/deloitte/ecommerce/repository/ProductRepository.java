8a51ba4715b178403347d48553ea578333a6fa75
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-02-01 09:23:51 -0300

    findbycateg

--- src/main/java/com/deloitte/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/deloitte/ecommerce/repository/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.deloitte.ecommerce.repository;
 
 import com.deloitte.ecommerce.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    //    todo metodo "findby" é considerado como "query" que ja nos da endpoint com o mesmo nome do metodo: search/findByCategoryId?id=1
+//    page é uma sublista
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
