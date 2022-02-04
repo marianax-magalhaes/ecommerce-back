02016b8ca2fe698d49990f5a12727a27403e8594
marianax-magalhaes <marianaximenes@poli.ufrj.br>
2022-02-04 10:55:29 -0300

    relacoes um para muito e o inverso setadas

--- src/main/java/com/deloitte/ecommerce/domain/Order.java
+++ src/main/java/com/deloitte/ecommerce/domain/Order.java
@@ -8,6 +8,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="orders")
@@ -38,6 +40,19 @@ public class Order {
     @Column(name="last_uptdated")
     @UpdateTimestamp
     private Date lastUpdated;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "order")
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+    public void add(OrderItem item){
+        if(item != null){
+            if(orderItems == null){
+                orderItems = new HashSet<>();
+            }
+            orderItems.add(item);
+            item.setOrder(this);
+        }
+    }
 
 
 }
--- src/main/java/com/deloitte/ecommerce/domain/OrderItem.java
+++ src/main/java/com/deloitte/ecommerce/domain/OrderItem.java
@@ -29,4 +29,6 @@ public class OrderItem {
     @Column(name="product_id")
     private Long productId;
 
+    private Order order;
+
 }
